8ecca139213643610cf4e0a772d0ae576f562372
Andreas Heissenberger <andreas@heissenberger.at>
2024-12-04 23:55:10 +0100

    update static rendering

--- src/pages/product/[id].static.ts
+++ src/pages/product/[id].static.ts
@@ -1,2 +1,6 @@
-export default [{ path: "/product/1" }, { path: "/product/2" }, { path: "/product/3" }];
+//export default [{ path: "/product/1" }, { path: "/product/2" }, { path: "/product/3" }];
+import { products } from '../../data/products'
+export default [{ id: 1 }, { id: 2 }, { id: 3 }];
+//export default  async () => products.map(p => ({ id: p.id}))
 
+//export default  () => products.map(p => ({ path: `/product/${p.id}` }));
